dbe831fc9f5b6f4ed603fa865c1d28eeae3f05cf
benni <benni@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-11 14:50:27 +0000

    Initial commit, taking over from seo_basics
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seo/trunk@12927 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.sql
@@ -0,0 +1,14 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	tx_seo_titletag varchar(100) DEFAULT '' NOT NULL,
+);
+
+#
+# Table structure for table 'pages_language_overlay'
+#
+CREATE TABLE pages_language_overlay (
+	tx_seo_titletag varchar(100) DEFAULT '' NOT NULL,
+);
+
